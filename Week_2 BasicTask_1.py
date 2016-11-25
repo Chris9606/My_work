fdc3d89b72a252f6e1c9d7c2998546cf0e94c074
Chris9606 <dimitr14@uni.coventry.ac.uk>
2016-11-25 23:47:54 +0000

    Add files via upload

---
+++ Week_2 BasicTask_1.py
@@ -0,0 +1,30 @@
+"""
+PERFECT_SQUARE(number)
+digit  0
+perfect_square  none
+
+while TRUE                      // Incrementing a digit starting from 1 until it's perfect square
+    digit  digit + digit      // is more than the given number and prints the previous perfect square
+    if digit**2 < number
+        perfect_square  digit**2
+    else
+        return perfect_square
+
+"""
+
+def perfect_square(number):
+    digit = 0
+    perfect_square = None
+
+    while True:                                           # Incrementing a digit starting from 1 until it's perfect square
+        digit += 1                                        # is more than the given number and prints the previous perfect square
+        if digit**2 < number:
+            perfect_square = digit**2
+        else:
+            print ("The closest perfect square is : ", perfect_square)
+            break
+
+number = int(input("Enter a number to find the closest perfect square which is less or equal to it : "))
+perfect_square(number)
+
+
