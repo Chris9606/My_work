8b9bf549c0ac338a7322eb31b93c016babfb3945
Chris9606 <dimitr14@uni.coventry.ac.uk>
2016-12-01 22:47:06 +0000

    Update Week_3 BasicTask_7.py

--- Week_3 BasicTask_7.py
+++ Week_3 BasicTask_7.py
@@ -4,28 +4,25 @@ PRIME_NUMBER_CHECK(number, count <-- 2)
     if check = 0 and number != count or number = 1
         OUTPUT "It is not a prime number!"
         return
-    elseif number = check
+    elseif number = count
         OUTPUT "It is a prime number!"
         return
 
     count <-- count + 1
     PRIME_NUMBER_CHECK(number, count)
 """
-def prime_number_check(number,count = 2):
+def prime_number_check(number,count = 2):            # Devides the number by 2 up to the value of the number.
     check = number % count
-    if check == 0 and  number != count or number == 1:
-        print ( "It's not a prime number")
+    if check == 0 and  number != count or number == 1:   # If The number devides by anything which is not itself or if the number is 1
+        print ( "It's not a prime number")               # It is NOT  a prime number
         return
-    elif number == check:
+    elif number == count:                            # Otherwise, it is.
         print ("It is a prime number")
         return
-    print (count)
     count+=1
     prime_number_check(number,count)
 
 prime_number_check(199)
-
-
 
 
 
