d4de58c9465cb9644ace4006b6cbc32e528a7447
Chris9606 <dimitr14@uni.coventry.ac.uk>
2016-12-01 22:51:54 +0000

    Update Week_2 BasicTask_3.py

--- Week_2 BasicTask_3.py
+++ Week_2 BasicTask_3.py
@@ -1,12 +1,12 @@
 """
 PERFECT_SQUARE(number)
-digit  0
-perfect_square  none
+digit <-- 0
+perfect_square <-- none
 
 while TRUE                      // Incrementing a digit starting from 1 until it's perfect square
-    digit  digit + digit      // is more than the given number and prints the previous perfect square
-    if digit**2 < number
-        perfect_square  digit**2
+    digit <-- digit + digit      // is more than the given number and prints the previous perfect square
+    if digit**2 <-- number
+        perfect_square <-- digit**2
     else
         return perfect_square
 
