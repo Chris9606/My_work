b1ee1612b1bc250ab3c24d0aad640e03632fb87b
Chris9606 <dimitr14@uni.coventry.ac.uk>
2016-12-02 17:57:53 +0000

    Update Week_5 BasicTask_10.py

--- Week_5 BasicTask_10.py
+++ Week_5 BasicTask_10.py
@@ -1,8 +1,6 @@
-
-
 def biggest_sequence (lst,count=0,output=[], A=0):
 
-    if A >= (len(lst) - 1):                                    # Breaks if all of the ements have been itereated over
+    if A >= (len(lst) - 1):                                    # Breaks if all of the elements have been iterated over
         print ("The list: ", lst)
         print("The bigget sequence is : ",max(output,key=len))  # max applies the key function to each element of your list and picks the one where the key function returned the biggest
         return
@@ -14,7 +12,7 @@ def biggest_sequence (lst,count=0,output=[], A=0):
         if i != lst[len(lst)-1] and  i < lst[lst.index(i)+1]: # If the element is not the last in the list and if it is less than the next element
             output[count].append(i)                           # Update the sequence 
             A +=1
-        else:                                                 # Else finlize this sequnce and start over, searching for another one
+        else:                                                 # Else finalize this sequence and start over, searching for another one
             output[count].append(i)
             A +=1
             count += 1
